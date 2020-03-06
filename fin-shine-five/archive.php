885d2ff2685d53f9a70d2eb2b89708884b7abb08
lijiahao <lijh@anchnet.com>
2020-03-06 13:50:42 +0800

    模版修改

--- fin-shine-five/archive.php
+++ fin-shine-five/archive.php
@@ -90,14 +90,23 @@ if ($category_slug != 'news' && $category_slug != 'productdetails') {
     $request_o = "SELECT $wpdb->terms.term_id, name, slug FROM $wpdb->terms ";
     $request_o .= " LEFT JOIN $wpdb->term_taxonomy ON $wpdb->term_taxonomy.term_id = $wpdb->terms.term_id  ";
     $request_o .= " WHERE  $wpdb->term_taxonomy.taxonomy = 'category' AND $wpdb->term_taxonomy.count != 0 ";
-    if ($p_array != '') {
-        $request_o .= " AND ( $wpdb->term_taxonomy.term_id not in ($p_s_array))";
-    }
-    if ($s_array != '') {
-        $request_o .= " AND ( $wpdb->term_taxonomy.term_id not in ($s_s_array))";
-    }
+    if (get_category($term_id)->slug == 'allclassification') {
+        if ($p_array != '') {
+            $request_o .= " AND ($wpdb->term_taxonomy.parent not in ($p_array) AND $wpdb->term_taxonomy.term_id not in ($p_array))";
+        }
+        if ($s_array != '') {
+            $request_o .= " AND ($wpdb->term_taxonomy.parent not in ($s_array) AND $wpdb->term_taxonomy.term_id not in ($s_array))";
+        }
+    } else {
+        if ($p_array != '') {
+            $request_o .= " AND ( $wpdb->term_taxonomy.term_id not in ($p_s_array))";
+        }
+        if ($s_array != '') {
+            $request_o .= " AND ( $wpdb->term_taxonomy.term_id not in ($s_s_array))";
+        }
 
-    $request_o .= " AND $wpdb->term_taxonomy.term_id in ($o_array)";
+        $request_o .= " AND $wpdb->term_taxonomy.term_id in ($o_array)";
+    }
     $request_o .= " ORDER BY term_id asc";
     $categorys_o = $wpdb->get_results($request_o);
     foreach ($categorys_o as $category) { //调用菜单
